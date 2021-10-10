8b08add03408f399b237eea3137a85b4139af5c9
deevee15 <vikulovd15@gmail.com>
2021-10-11 00:24:49 +0300

    ver 1

---
+++ index.php
@@ -0,0 +1,198 @@
+<head>
+    <title>1</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta name="theme-color" content="#000">
+</head>
+<body>
+    <div id="header">
+        <a href="#main_mob"><div class="header_logo"><img src="img/logo.png"></div></a>
+    </div>
+    <div id="main">
+        <div class="main_content">
+            <div class="main_content_1">
+                <div class="main_content_logo"><img src="img/logo.png"></div>
+                <div class="main_content_bigtext">Киберспортивный компьютерный клуб премиум-класса.</div>
+                <div class="main_content_text1">
+                    <p>Здесь вас ждут</p>
+                    <ul>
+                        <li>Регулярные соревнования</li>
+                        <li>Вежливый и приветливый персонал</li>
+                    </ul>
+                </div>
+                <div class="main_content_text2">
+                    <p>а самое главное</p>
+                    <p>Мощнейшие ПК с самым современным железом.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="main_mob">
+        <div class="main_mob_bg">
+            <div class="main_mob_bg_bigtext">Киберспортивный компьютерный клуб премиум-класса.</div>
+        </div>
+        <div class="main_mob_part1">
+            <p>Здесь вас ждут</p>
+            <ul>
+                <li>Регулярные соревнования</li>
+                <li>Вежливый и приветливый персонал</li>
+            </ul>
+        </div>
+        <div class="main_mob_part2">
+            <p>а самое главное</p>
+            <p>Мощнейшие ПК с самым современным железом.</p>
+        </div>
+    </div>
+    <div id="video">
+        <div class="video_overlay"></div>
+        <div class="video_content">
+            <div class="video_content_button"><svg width="55" height="65" viewBox="0 0 55 65" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M55 32.5L0.249997 64.1099L0.25 0.890069L55 32.5Z" fill="white"/></svg></div>
+            <div class="video_content_1">
+                <iframe width="1178" height="662" src="https://www.youtube.com/embed/CSw74FmXg-A" frameborder="0" allowfullscreen></iframe>
+                <img src="img/video_vid.png">
+            </div>
+        </div>
+    </div>
+    <div id="ref">
+        <div class="ref_content">
+            <div class="ref_content_pic">
+                <img src="img/ref_pic.png">
+            </div>
+            <div class="ref_content_bigtext">Приводи друзей в Godji Game и построй собственную реферальную сеть!</div>
+            <div class="ref_content_pic_mob">
+                <img src="img/ref_pic.png">
+            </div>
+            <div class="ref_content_percent">
+                <svg width="167" height="106" viewBox="0 0 167 106" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.036 0.194182V19.4342H20.756V103.794H44.732V0.194182H0.036ZM98.249 39.1182H89.813L91.441 19.4342H134.657V0.194182H71.461L66.133 58.5062H92.625C111.421 58.5062 116.749 63.8342 116.749 71.9742C116.749 80.4102 109.349 85.4422 97.361 85.4422C86.557 85.4422 75.753 82.0382 67.909 76.1182L58.437 94.4702C68.501 101.574 83.597 105.57 97.953 105.57C127.553 105.57 141.021 89.5862 141.021 71.2342C141.021 52.8822 128.885 39.1182 98.249 39.1182ZM149.825 96.1142C152.369 96.1142 154.289 94.4342 154.289 91.4342C154.289 88.4582 152.369 86.7542 149.825 86.7542C147.281 86.7542 145.361 88.4582 145.361 91.4342C145.361 94.4342 147.281 96.1142 149.825 96.1142ZM148.217 103.794H152.273L163.745 86.9942H159.689L148.217 103.794ZM149.825 93.5222C149.297 93.5222 148.769 93.0422 148.769 91.4342C148.769 89.8262 149.297 89.3462 149.825 89.3462C150.353 89.3462 150.881 89.8262 150.881 91.4342C150.881 93.0422 150.353 93.5222 149.825 93.5222ZM162.137 104.034C164.681 104.034 166.601 102.354 166.601 99.3542C166.601 96.3782 164.681 94.6742 162.137 94.6742C159.593 94.6742 157.673 96.3782 157.673 99.3542C157.673 102.354 159.593 104.034 162.137 104.034ZM162.137 101.442C161.609 101.442 161.081 100.962 161.081 99.3542C161.081 97.7462 161.609 97.2662 162.137 97.2662C162.665 97.2662 163.193 97.7462 163.193 99.3542C163.193 100.962 162.665 101.442 162.137 101.442Z" fill="url(#paint0_linear)"/>
+<defs>
+<linearGradient id="paint0_linear" x1="35.5714" y1="-45.2058" x2="126.35" y2="62.5879" gradientUnits="userSpaceOnUse">
+<stop offset="0.390625" stop-color="white"/>
+<stop offset="0.975306" stop-color="#298BD1"/>
+</linearGradient>
+</defs>
+</svg>
+                <p>Ты получишь с суммы пополнения</p>
+            </div>
+            <div class="ref_content_iphone">
+                <img src="img/ref_iphone.png">
+                <div class="ref_content_iphone_title">iPhone 12!</div>
+                <p>Самые активные смогут забрать</p>
+            </div>
+        </div>
+    </div>
+    <div id="nurm1">
+        <div class="nurm_text">
+            <div class="nurm1_text1">От Нурминского?</div>
+            <div class="nurm1_text2">Да? В таком случае листай ниже и получай бесплатный час за ПК в нашем клубе.</div>
+        </div>
+    </div>
+    <div id="nurm2">
+        <div class="nurm2_content">
+            <div class="nurm2_content_bigtext">Мы сами не ожидали, что Нурминский любит клуб Godji Game и участвует в нашей реферальной программе. </div>
+            <div class="nurm2_content_middletext">
+                <p><font style="color:#C2DBFF;">Если ты хочешь присоединиться к рефералам исполнителя и получить час -</font> необходимо выполнить 2 действия: </p>
+                <ol>
+                    <li>Зарегистрироваться в нашем клубе</li>
+                    <li>Написать в нашем Telegram-боте<br> «Я от Нурминского»</li>
+                </ol>
+            </div>
+            <div class="nurm2_content_nurmpic">
+                <img src="img/nurminskiy.png">
+            </div>
+        </div>
+    </div>
+    <div id="banner">
+        <p>И вот уже час на твоем счете!  <font>Листай ниже и получи его!</font></p>
+    </div>
+    <div id="city">
+        <div class="city_content">
+            <div class="city_content_text">Выберите город, в котором хотите играть!</div>
+            <div class="city_content_cities">
+                <ul>
+                    <a href="https://vk.com/izhevsk_gg"><li>Ижевск</li></a>
+                    <a class="city_content_cities_kazan"><li>Казань</li></a>
+                    <a href="http://chelnygg.ru"><li>Набережные Челны</li></a>
+                    <a href="http://nkgg.ru"><li>Нижнекамск</li></a>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div id="telegram">
+        <div class="telegram_content">
+            <div class="telegram_content_text">Переходите в Telegram-бота </div>
+            <div class="telegram_content_cities">
+                <ul>
+                    <a href="https://vk.com/izhevsk_gg"><li>Ижевск</li></a>
+                    <a class="telegram_content_cities_kazan"><li>Казань</li></a>
+                    <a href="https://t.me/GoodGameChelnyBot"><li>Набережные Челны</li></a>
+                    <a href="https://t.me/GoodGameNKBot"><li>Нижнекамск</li></a>
+                </ul>
+            </div>
+            <div class="telegram_content_text2">и забирайте час за ПК!</div>
+        </div>
+    </div>
+    <div id="select_city">
+        <div class="select_city_layout"></div>
+        <div class="select_city_ask">На какой улице находится ваш клуб?</div>
+        <div class="select_city_list scl_kazan">
+            <ul>
+                <a href="https://good-game.club" class="scl_kazan_dz"><li>ул. Дзержинского 5</li></a>
+                <a href="https://gorkigg.ru" class="scl_kazan_mavl"><li>ул. Хусаина Мавлютова 17е к.2</li></a>
+                <a href="https://azinogg.ru" class="scl_kazan_glushko"><li>ул. Академика Глушко 41а</li></a>
+                <a href="https://mosgg.ru/" class="scl_kazan_volg"><li>ул. Волгоградская 18</li></a>
+            </ul>
+            <ul>
+                <a href="https://salavatgg.ru" class="scl_kazan_shak"><li>ул. Ильгама Шакирова 5</li></a>
+                <a href="https://volkovagg.ru" class="scl_kazan_volk"><li>ул. Волкова 60/12</li></a>
+            </ul>
+        </div>
+        <div class="select_city_close"><svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1.70711 1L17.7071 17M1 17L17 1"/>
+</svg>
+</div>
+    </div>
+</body>
+<script type="text/javascript">
+    if($(window).width() < '1280'){
+        var width=$(window).width();var height=$(window).height();
+        $('.video_content_1 iframe').attr('width','330');$('.video_content_1 iframe').attr('height','186');
+        $('.video_content_button svg').attr('width','21');$('.video_content_button svg').attr('height','22');
+    }
+</script>
+<script type="text/javascript">
+    $('.video_content_1 iframe').hide();
+    $('.video_content_button').click(function(){
+        $(this).hide();
+        $('.video_content_1 iframe').show();
+        $('.video_content_1 iframe').attr('src','https://www.youtube.com/embed/CSw74FmXg-A?autoplay=1');
+        $('.video_content_1 img').hide();
+    });
+    $('.scl_kazan').hide();
+    $('.city_content_cities_kazan').click(function(){
+        $('.scl_kazan').show();
+        $('.scl_kazan_dz').attr('href','https://good-game.club');
+        $('.scl_kazan_mavl').attr('href','https://gorkigg.ru');
+        $('.scl_kazan_glushko').attr('href','https://azinogg.ru');
+        $('.scl_kazan_volg').attr('href','https://mosgg.ru/');
+        $('.scl_kazan_shak').attr('href','https://salavatgg.ru');
+        $('.scl_kazan_volk').attr('href','https://volkovagg.ru');
+        $('#select_city').addClass('show');
+    });
+    $('.select_city_close').click(function(){$('#select_city').removeClass('show');});
+    $('.telegram_content_cities_kazan').click(function(){
+        $('.scl_kazan').show();
+        $('.scl_kazan_dz').attr('href','https://t.me/GoodGameKznBot');
+        $('.scl_kazan_mavl').attr('href','https://t.me/GoodGameMavBot');
+        $('.scl_kazan_glushko').attr('href','https://t.me/GoodGameAZINOBot');
+        $('.scl_kazan_volg').attr('href','https://t.me/GoodGameVOLGBot');
+        $('.scl_kazan_shak').attr('href','https://t.me/GoodGameSalBot');
+        $('.scl_kazan_volk').attr('href','https://t.me/GoodGameVolkovaBot');
+        $('#select_city').addClass('show');
+    });
+</script>
